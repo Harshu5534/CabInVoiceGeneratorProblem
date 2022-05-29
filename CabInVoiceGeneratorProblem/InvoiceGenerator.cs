4af8997686ea6b83f4335f2f6732a97267d31243
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-05-29 21:17:45 +0530

    [Harshal] Add:Display Invoice Summary For Specific UserId

--- CabInVoiceGeneratorProblem/InvoiceGenerator.cs
+++ CabInVoiceGeneratorProblem/InvoiceGenerator.cs
@@ -13,6 +13,7 @@ namespace CabInVoiceGeneratorProblem
         const int MIN_FARE = 5;
         const int FARE_PER_KM = 10;
         const int FARE_PER_MIN = 1;
+        RideRepository repo = new RideRepository();
         public double CalculateFare(int distance, int time)
         {
             double calulateFare = distance * FARE_PER_KM + time * FARE_PER_MIN;
@@ -36,6 +37,15 @@ namespace CabInVoiceGeneratorProblem
                 result += CalculateFare(data.distance, data.time);
             }
             return new InvoiceSummary(rides.Length, result);
+        }
+        public void MapUserId(string userId,Ride[] rides)
+        {
+            this.repo.Addrides(userId,rides);   
+        }
+        public InvoiceSummary GetInvoiceSummary(string userId)
+        {
+            Ride[] result=this.repo.GetRides(userId);
+            return MultipleRides(result);
         }
     }
 }
---
+++ CabInVoiceGeneratorProblem/RideRepository.cs
@@ -0,0 +1,33 @@
+﻿using CabInVoiceGeneratorProblem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class RideRepository
+    {
+        Dictionary<string, Ride[]> rideDict = new Dictionary<string, Ride[]>();
+        public void Addrides(string userId,Ride[] rides)
+        {
+            if(!rideDict.ContainsKey(userId))
+            {
+                rideDict.Add(userId,rides);
+            }
+        }
+        public Ride[] GetRides(string userId)
+        {
+            foreach(var data in rideDict)
+            {
+                if (data.Key == userId)
+                {
+                    return data.Value;
+                }
+            }
+            return null;
+        }
+
+    }
+}
