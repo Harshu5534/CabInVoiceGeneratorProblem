51375dcde74803425ac1091c5f92f812b2c89f61
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-05-30 15:17:45 +0530

    [Harshal] Add:Display Total Fare Premium Rides

--- CabInVoiceGeneratorProblem/InvoiceSummary.cs
+++ CabInVoiceGeneratorProblem/InvoiceSummary.cs
@@ -17,5 +17,16 @@ namespace CabInvoiceGenerator
             this.totalFare = totalFare;
             this.AvgFairPerRide = this.totalFare / this.totalNumberOfRides;
         }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) ; return false;
+            if (!(obj is InvoiceSummary)) return false;
+            InvoiceSummary inputesObject = (InvoiceSummary)obj;
+            return this.totalNumberOfRides == inputesObject.totalNumberOfRides && this.totalFare == inputesObject.AvgFairPerRide && this.AvgFairPerRide == inputesObject.AvgFairPerRide;
+        }
+        public override int GetHashCode()
+        {
+            return this.totalNumberOfRides.GetHashCode() ^ this.totalFare.GetHashCode() ^ this.AvgFairPerRide.GetHashCode();
+        }
     }
 }
--- InvoiceTester/InvoiceTest.cs
+++ InvoiceTester/InvoiceTest.cs
@@ -9,14 +9,14 @@ namespace InvoiceTester
         [Test]
         public void InputInInteger_ShouldReturn_SingleRides_TotalFare()
         {
-            InvoiceGenerator invoice = new InvoiceGenerator();
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL);
             double actual = invoice.CalculateFare(6, 5);
             Assert.AreEqual(actual, 65);
         }
         [Test]
         public void InputInInteger_ShouldReturn_MultipleRides_TotalFare()
         {
-            InvoiceGenerator invoice = new InvoiceGenerator();
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.PREMIUM);
             Ride[] rides = { new Ride(2, 3), new Ride(4, 5) };
             double result = invoice.CalculateMultipleRides(rides);
             Assert.AreEqual(result, 23, 45);
@@ -24,7 +24,7 @@ namespace InvoiceTester
         [Test]
         public void InputInInteger_ShouldReturn_MultipleRides_TotalFair_InvoiceSummary()
         {
-            InvoiceGenerator invoice = new InvoiceGenerator();
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL);
             Ride[] rides = { new Ride(2, 3), new Ride(4, 5), new Ride(5, 6) };
             InvoiceSummary result = invoice.MultipleRides(rides);
             Assert.AreEqual(result.totalNumberOfRides, rides.Length);
@@ -32,11 +32,19 @@ namespace InvoiceTester
         [Test]
         public void InputInString_GivenUserId_ShouldReturn_MultipleRides_TotalFair_InvoiceSummary()
         {
-            InvoiceGenerator invoice = new InvoiceGenerator();
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL);
             Ride[] rides = { new Ride(2, 3), new Ride(4, 5), new Ride(5, 6) };
             invoice.MapUserId("Harshu27", rides);
             InvoiceSummary summary = invoice.GetInvoiceSummary("Harshu27");
             Assert.AreEqual(summary.totalNumberOfRides, 3);
+        }
+        [Test]
+        public void InputInInteger_ShouldReturn_MultipleRides_TotalFair_InvoiceSummary_ForPremiumRides()
+        {
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.PREMIUM);
+            Ride[] preRides = { new Ride(15, 10), new Ride(35, 35), new Ride(25, 15), new Ride(15, 15), new Ride(50, 60) };
+            InvoiceSummary result = invoice.MultipleRides(preRides);
+            Assert.AreEqual(result.totalNumberOfRides, preRides.Length);
         }
     }
 }
